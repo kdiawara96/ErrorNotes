730288e0e458d591181e96d2af0cc7252ff34689
kdiawara96 <karimdiawara96@gmail.com>
2022-08-26 12:01:20 +0000

    La création des models ainsi que des repository

---
+++ src/main/java/com/YouKar/ml/ErrorNotes/Models/Problemes.java
@@ -0,0 +1,4 @@
+package com.YouKar.ml.ErrorNotes.Models;
+
+public class Problemes {
+}
---
+++ src/main/java/com/YouKar/ml/ErrorNotes/Repository/Solutions_repo.java
@@ -0,0 +1,7 @@
+package com.YouKar.ml.ErrorNotes.Repository;
+
+import com.YouKar.ml.ErrorNotes.Models.Solutions;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Solutions_repo extends JpaRepository<Solutions, Long> {
+}
