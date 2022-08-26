326c70f87c529576940358876e4f9d5313658309
kdiawara96 <karimdiawara96@gmail.com>
2022-08-26 09:16:08 +0000

    This is the project to ErrorNotes

---
+++ src/main/java/com/YouKar/ml/ErrorNotes/ErrorNotesApplication.java
@@ -0,0 +1,13 @@
+package com.YouKar.ml.ErrorNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/YouKar/ml/ErrorNotes/ErrorNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YouKar.ml.ErrorNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
